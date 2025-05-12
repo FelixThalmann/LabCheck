05869de6c08db971197b9268335b08926388c021
Felix Thalmann <felix.thalmann@online.de>
2025-05-12 12:59:06 +0200

    frontend: add background and header

--- frontend/lib/features/home/presentation/pages/home_page.dart
+++ frontend/lib/features/home/presentation/pages/home_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import '../widgets/header_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('LabCheck')),
-      body: const Center(child: Text('Willkommen bei LabCheck')),
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.50, -0.00),
+            end: Alignment(0.50, 1.00),
+            colors: [Color(0xFF599CD1), Color(0xFF88B8DE), Color(0xFFFEFEFE)],
+          ),
+        ),
+        child: Column(children: const [HeaderWidget()]),
+      ),
     );
   }
 }
---
+++ frontend/lib/features/home/presentation/widgets/header_widget.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final headerHeight = screenHeight * 0.25;
+    final logoSize = screenWidth * 0.24;
+    final logoHeight = logoSize * 0.3;
+
+    return Container(
+      width: screenWidth,
+      height: headerHeight,
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+      child: Stack(
+        children: [
+          Positioned(
+            right: screenWidth * 0.02,
+            top: headerHeight * 0.42,
+            child: Container(
+              width: logoSize,
+              height: logoHeight,
+              child: SvgPicture.asset(
+                'assets/images/ubicomp-logo.svg',
+                width: logoSize,
+                height: logoHeight,
+                fit: BoxFit.contain,
+                colorBlendMode: BlendMode.srcIn,
+                placeholderBuilder:
+                    (BuildContext context) => Container(
+                      width: logoSize,
+                      height: logoHeight,
+                      color: Colors.grey[300],
+                    ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: screenWidth * 0.05,
+            top: headerHeight * 0.4,
+            child: SizedBox(
+              width: screenWidth * 0.6,
+              child: Text(
+                'Ubiquitous Systems Lab',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: screenWidth * 0.045,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                  height: 0.85,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: screenWidth * 0.05,
+            top: headerHeight * 0.5,
+            child: SizedBox(
+              width: screenWidth * 0.6,
+              child: Text(
+                'May I com in?',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: screenWidth * 0.025,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                  height: 2,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
