165f76b4a4dc0301848911caa565f4f3a6ecb22f
Jappaha <140712936+Jappaha@users.noreply.github.com>
2025-05-18 14:05:13 +0200

    wifi settings funktion
    + added wifi configuration option

--- hardware/LabCheckFirmware/src/main.cpp
+++ hardware/LabCheckFirmware/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Preferences.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
@@ -12,9 +13,12 @@ const int timeout = 20000;
 char menuOption = 0;
 long time0;
 bool sensorToggle = 0;
+Preferences prefs;
 
-const char* SSID = "KultanaPC";
-const char* PASSWORD = "a1b2c3qwertzuiop";
+const String DEF_SSID = "HotSpot US640235";
+const String DEF_PASSWORD = "U-Sie!6402";
+String ssid;
+String password;
 
 int song[6] = {1209,0,1209,0,0,0};
 int songCounter = 0;
@@ -31,23 +35,28 @@ int noteDuration = 75;
 int melodyCounter = 0;
 
 char menu();
+void menuInstructions();
 void connectToWifi();
+void setupWiFi();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   while(!Serial);
+
   pinMode(MAGNETIC, INPUT_PULLUP);
   pinMode(SIGNALLED, OUTPUT);
+  prefs.begin("settings", false);
+  ssid = prefs.getString("ssid", DEF_SSID);
+  password = prefs.getString("password", DEF_PASSWORD);
   //save tone init
   tone(SPEAKER, 100,10);
   delay(15);
   noTone(SPEAKER);
 
 
-
   Serial.println("ESP32 gestartet!");
-
+  menuInstructions();
   menuOption = menu();
 }
 
@@ -88,6 +97,11 @@ void loop() {
     menuOption = menu();
   }
 
+  if(menuOption == '4'){
+    setupWiFi();
+    menuOption = menu();
+  }
+
   if (Serial.available()){
     char c = Serial.read();
     if (c == 'c'){
@@ -103,23 +117,20 @@ void loop() {
 
 // Menu funktion for selecting the components to be tested
 char menu(){
-  Serial.println(F("\nWelcher Komponent soll getestet werden?"));
-  Serial.println(F("(1) Magnetischer Tuer Sensor Set"));
-  Serial.println(F("(2) Geheimer Song"));
-  Serial.println(F("(3) WiFi Test: Verbinden und IP Adresse ausgeben"));
-  Serial.println(F("(menu) sende etwas Anderes oder druecke den Board Reset Knopf\n"));
+  Serial.print(F("Input option: "));
   while(!Serial.available());
-
   // Read data from serial monitor if received
   while(Serial.available()){
     char c = Serial.read();
     if(isAlphaNumeric(c)){
       if(c == '1'){
-        Serial.println(F("Teste nun 'Magnetischer Tuer Sensor Set'."));
+        Serial.println(F("Testing 'Magnetic door sensor set'."));
       } else if (c == '2'){
         Serial.println(F("Spiele nun Tales Song..."));
       } else if (c == '3'){
         Serial.println(F("WiFi Verbindung..."));
+      } else if (c == '4'){
+        Serial.println(F("WiFi Setup"));
       } else if (c == 'c'){
         Serial.println(F("Zurueck ins Menu..."));
       } else {
@@ -133,29 +144,61 @@ char menu(){
   return 0;
 }
 
-void connectToWifi(){
-  Serial.print(F("Verbinde in 3..."));
-  delay(3000);
+// menu instructions in english
+void menuInstructions(){
+  Serial.println(F("\nWhich component should be tested?"));
+  Serial.println(F("(1) Magnetic Door Sensor Set"));
+  Serial.println(F("(2) Secret Song"));
+  Serial.println(F("(3) WiFi Test: Connect and print IP address"));
+  Serial.println(F("(4) WiFi Setup: Set SSID and Password"));
+  Serial.println(F("(menu) send something else or press the board reset button\n"));
+}
 
+void connectToWifi(){
+  Serial.print(F("Connecting to "));
+  Serial.print(F(ssid.c_str()));
+  Serial.print(F("..."));
   WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
+  WiFi.begin(ssid.c_str(), password.c_str());
   int timecounter = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(F("."));
-    digitalWrite(SIGNALLED, HIGH);
-    delay(100);
-    digitalWrite(SIGNALLED, LOW);
-
+    for (int i = 0; i < 2; i++){
+      digitalWrite(SIGNALLED, HIGH);
+      delay(100);
+      digitalWrite(SIGNALLED, LOW);
+      delay(100);
+    }
     if(timecounter > 10){
       Serial.println(F("Timeout!"));
+      tone(SPEAKER, 800, 50);
+      tone(SPEAKER, 200, 100);
       return;
     }
     timecounter++;
   }
-  Serial.println(F("\nVerbunden!"));
+  Serial.println(F("Connected!"));
   Serial.print(F("IP Adresse: "));
   Serial.println(WiFi.localIP().toString().c_str());
-  tone(SPEAKER, 800, 10);
-  tone(SPEAKER, 1050, 10);
+  tone(SPEAKER, 800, 50);
+  tone(SPEAKER, 1050, 100);
+}
+
+// set the ssid and password by serial, save it to the preferences
+void setupWiFi(){
+  Serial.setTimeout(10000);
+  Serial.print(F("Input SSID: "));
+  while(!Serial.available());
+  ssid = Serial.readStringUntil('\n');
+  prefs.putString("ssid", ssid);
+  Serial.println(F(ssid.c_str()));
+  
+  Serial.print(F("Input password: "));
+  while(!Serial.available());
+  password = Serial.readStringUntil('\n');
+  prefs.putString("password", password);
+  Serial.println(F("OK"));
+  Serial.setTimeout(1000);
+  Serial.println(F("Setup completed."));
 }
