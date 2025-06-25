e9c51ab42c1ea83a31d56b6c51552598d5582343
Felix Thalmann <felix.thalmann@online.de>
2025-06-25 19:57:38 +0200

    frontend: bugfix websockets, where initialization is done twice

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,7 +6,6 @@ import 'features/setting/presentation/pages/settings_page.dart';
 import 'features/about/presentation/pages/about_page.dart';
 import 'core/logger/app_logger.dart';
 import 'core/config/environment_config.dart';
-import 'data/services/websocket_service.dart';
 
 void main() async {
   AppLogger.init();
@@ -14,9 +13,6 @@ void main() async {
 
   await EnvironmentConfig.initialize();
   EnvironmentConfig.printAllVariables();
-
-  // Initialize WebSocket service globally
-  WebSocketService().initialize();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
